01824964755ada354871d7bbe11fc308a6b6221c
nicolly <saantos.nick1606@gmail.com>
2026-03-20 16:01:20 -0300

    feat: finaliza arquivo products

---
+++ assets/data/products.js
@@ -0,0 +1,74 @@
+export const products = [
+  {
+    id: 1,
+    nome: 'Tonico Creamy',
+    categoria: 'Skin Care',
+    preco:'49,99',
+    descricao:'Tônico facial para limpeza e equilíbrio da pele.',
+    imagem: "https://www.drogasil.com.br/_next/image?url=https%3A%2F%2Fproduct-data.raiadrogasil.io%2Fimages%2F3516814.webp&w=3840&q=40"
+    },
+    {
+    id: 2,
+    nome: 'Creme Firmador Anti Aging',
+    categoria: 'Skin Care',
+    preco:'69,99',
+    descricao:'Creme firmador para redução de rugas e melhora da elasticidade da pele.',
+    imagem: "https://farmaconde.vtexassets.com/arquivos/ids/213190-1200-675?v=638615953837130000&width=1200&height=675&aspect=true"
+  },
+  {
+    id: 3,
+    nome: 'Duo Ácido',
+    categoria: 'Skin Care',
+    preco:'59,99',
+    descricao:'Duo para exfoliação e hidratação da pele.',
+    imagem: "https://http2.mlstatic.com/D_Q_NP_667068-MLA82950774066_032025-F.webp"
+  },
+  {
+    id: 4,
+    nome: 'Blush Bastão',
+    categoria: 'Makeup',
+    preco: 'R$ 85,22',
+    descricao: 'Blush/batom monocromático para olhos, bochechas e lábios.',
+    imagem: 'https://cdn-storage-r2.boxloja.pro/k7ljjpwhq343l1729vbjnir18y8n',
+  },
+  {
+    id: 5,
+    nome: 'Base Líquida',
+    categoria: 'Makeup',
+    preco: 'R$ 39,99',
+    descricao: 'Base líquida para cobertura total e acabamento natural.',
+    imagem: "https://m.media-amazon.com/images/I/41ugDjP-TmL._AC_SY300_SX300_QL70_ML2_.jpg",
+  },
+  {
+    id: 6,
+    nome: 'Lip Juice',
+    categoria: 'Makeup',
+    preco: 'R$ 119,90',
+    descricao: 'Lip tint com acabamento vibrante.',
+    imagem: "https://marimaria.vtexassets.com/arquivos/ids/168372/morango-lipjuice-fundo-mmk.png?v=638955427197130000",
+  },
+    {
+    id: 7,
+    nome: 'Kit Ybera',
+    categoria: 'Hair',
+    preco: 'R$ 119,90',
+    descricao: 'Kit de tratamento capilar para cabelos danificados.',
+    imagem: "https://images.tcdn.com.br/img/img_prod/789677/kit_mirracura_com_oleo_de_mirra_90ml_ybera_paris_2523_1_42f030a845bff7def29a203b94633e83.jpg",
+    },
+    {
+    id: 8,
+    nome: 'Kit Eudora Siage',
+    categoria: 'Hair Care',
+    preco: 'R$ 139,90',
+    descricao: 'Kit completo para reconstrução dos fios.',
+    imagem: "https://m.media-amazon.com/images/I/51lwNZQQRUL._AC_UF350,350_QL80_.jpg",
+    },
+    {
+    id: 9,
+    nome: 'Oléo Finalizador',
+    categoria: 'Hair',
+    preco: 'R$ 34,90',
+    descricao: 'Óleo finalizador para brilho e controle do frizz.',
+    imagem: "https://drogal.vtexassets.com/arquivos/ids/216374-1200-900?v=638487835555330000&width=1200&height=900&aspect=true",
+    },
+];
